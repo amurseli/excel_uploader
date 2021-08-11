853faaaffa120080f031d33a26a02af0cb22326e
amurseli <amurseli@fi.uba.ar>
2021-08-11 05:07:25 -0300

    Empezado el módulo de automatización de dáis libres

--- excel_edit.py
+++ excel_edit.py
@@ -6,6 +6,8 @@ path = askopenfilename(title = "Seleccione el archivo que quiere editar")
 path_split = path.split("/") # Crea una lista con las partes del path
 file_name = path_split[-1] # y guarda el último de los elementos en una variable
 
+DIC_TIERS = {"TIER_1": [1, 10],"TIER_2": [5, 15],"TIER_3": [10, 20],"TIER_4": [15, 35],"TIER_5": [20, 42], }
+
 def get_header(path):
     with open (path, "r", newline="") as f:
         csv_reader = csv.reader(f)
@@ -14,7 +16,7 @@ def get_header(path):
         return header
 
 def writer(header, data, path):
-    with open (path, "w", newline = "") as f:
+    with open ("new_Excel", "w", newline = "") as f:
         writer = csv.DictWriter(f, fieldnames = header)
         writer.writeheader()
         writer.writerows(data)
@@ -27,13 +29,30 @@ def choose_column(path) -> tuple:
     column = header[choice - 1]
     return (header, column)
 
+def choose_column_free_day(path) -> tuple:
+    header = get_header(path)
+    for i in range(len(header)):
+        print(f"{i + 1} - {header[i]}")
+    choice = int(input("Elija la Columna que contenga los años de antiguedad: "))
+    column = header[choice - 1]
+    return (header, column)   
+
 def updater(path):
     header, column = choose_column(path)
     with open(path, newline= "") as f:
         readData = [row for row in csv.DictReader(f)]
         for i in range(len(readData)):
-            newRating = input("Ingrese el Nuevo Valor: ")
-            readData[i][column] = newRating
+            newValue = input("Ingrese el Nuevo Valor: ")
+            readData[i][column] = newValue
     writer(header, readData, path)
+
+def free_day_update(path):
+    header, column_years = choose_column_free_day(path)
+    with open(path, newline= "") as f:
+        readData = [row for row in csv.DictReader(f)]
+        for i in range(len(readData)):
+            if int(readData[i][column_years]) < DIC_TIERS["TIER_1"]:
+               pass #Falta la condicion de que los dias no hayan sido cambiados. 
+
 
 updater(path)
